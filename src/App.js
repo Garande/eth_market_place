7dc2b928cd5b98be808f38dc13d7a93834f27582
Garande Abdalah Hassani (Wafula) <hassaniwafula@gmail.com>
2021-03-29 10:30:46 +0300

    market frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Web3 from 'web3';
+import Navbar from './components/Navbar';
+import Marketplace from './abis/Marketplace.json';
+
+import Market from './components/Market'
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accounts: [],
+      account: '',
+      productCount: 0,
+      products: [],
+      loading: true,
+    }
+    this.createProduct = this.createProduct.bind(this)
+  }
+
+
+  async componentWillMount() {
+    window.addEventListener('load', async () => {
+      await this.loadWeb3();
+      await this.loadBlockchainData()
+    })
+
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      try {
+        await window.ethereum.enable()
+      } catch (error) {
+        console.error('Conn Error::', error);
+      }
+
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    } else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+
+
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    console.log('Accounts::', accounts);
+    this.setState({ account: accounts[0] });
+    const networkId = await web3.eth.net.getId();
+
+    console.log('Network Id::', networkId)
+    const networkData = Marketplace.networks[5777]
+    console.log('Network data::', networkData)
+
+    if (networkData) {
+      const marketplace = new web3.eth.Contract(Marketplace.abi, networkData.address);
+      console.log('Marketplace::', marketplace);
+      this.setState({ marketplace });
+      // const productCount = await marketplace.methods.productCount().call();
+      // console.log('Product Count::', productCount)
+      this.setState({ loading: false })
+
+    } else {
+      window.alert('Marketplace contract not deployed to detected network.')
+    }
+  }
+
+
+  createProduct(name, price) {
+    this.setState({ loading: true });
+    this.state.marketplace.methods.createProduct(name, price).send({ from: this.state.account }).once('receipt', (receipt) => {
+      this.setState({ loading: false });
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar account={this.state.account} />
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex">
+              {this.state.loading ? <div id="loader" className="text-center">
+                <p className="text-center">Loading...</p>
+              </div> : <Market createProduct={this.createProduct} />}
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/contracts/Marketplace.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.5.0;
+
+contract Marketplace {
+    string public name;
+    uint256 public productCount = 0;
+    mapping(uint256 => Product) public products;
+
+    constructor() public {
+        name = "Garande Marketplace";
+    }
+
+    struct Product {
+        uint256 id;
+        string name;
+        uint256 price;
+        address payable owner;
+        bool purchased;
+    }
+
+    event ProductCreated(
+        uint256 id,
+        string name,
+        uint256 price,
+        address payable owner,
+        bool purchased
+    );
+
+    event ProductPurchased(
+        uint256 id,
+        string name,
+        uint256 price,
+        address payable owner,
+        bool purchased
+    );
+
+    function createProduct(string memory _name, uint256 _price) public {
+        // Require a valid name
+        require(bytes(_name).length > 0);
+        // Require a valid price
+        require(_price > 0);
+        // Increment product count
+        productCount++;
+        // Create the product
+        address payable sender = address(uint160(msg.sender));
+
+        //  payable(uint160(msg.sender));
+
+        // address sender = msg.sender;
+
+        products[productCount] = Product(
+            productCount,
+            _name,
+            _price,
+            sender,
+            false
+        );
+        // Trigger an event
+        emit ProductCreated(productCount, _name, _price, sender, false);
+    }
+
+    function purchaseProduct(uint256 _id) public payable {
+        address payable sender = address(uint160(msg.sender));
+        // payable(msg.sender);
+
+        // address sender = msg.sender;
+
+        // Fetch the product
+        Product memory _product = products[_id];
+        // Fetch the owner
+        address payable _seller = _product.owner;
+        // Make sure the product has a valid id
+        require(_product.id > 0 && _product.id <= productCount);
+        // Require that there is enough Ether in the transaction
+        require(msg.value >= _product.price);
+        // Require that the product has not been purchased already
+        require(!_product.purchased);
+        // Require that the buyer is not the seller
+        require(_seller != msg.sender);
+        // Transfer ownership to the buyer
+        _product.owner = sender;
+        // Mark as purchased
+        _product.purchased = true;
+        // Update the product
+        products[_id] = _product;
+        // Pay the seller by sending them Ether
+        address payable seller = _seller;
+        // address seller = _seller;
+        seller.transfer(msg.value);
+        // Trigger an event
+        emit ProductPurchased(
+            productCount,
+            _product.name,
+            _product.price,
+            sender,
+            true
+        );
+    }
+}
